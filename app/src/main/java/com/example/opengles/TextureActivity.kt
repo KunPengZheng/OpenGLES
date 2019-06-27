21706149bd0774767bcf4a2787a1d2f4cf384386
KunPengZheng <zhengwp@adnonstop.com>
2019-06-27 10:29:54 +0800

    删除无效代码

--- app/src/main/java/com/example/opengles/TextureActivity.kt
+++ app/src/main/java/com/example/opengles/TextureActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -20,10 +19,6 @@ import com.example.opengles.renderer.TextureRenderer
 class TextureActivity : AppCompatActivity() {
 
     private var glSurfaceView: GLSurfaceView? = null
-    private var nonMatrixBtn: Button? = null
-    private var matrix_4_3Btn: Button? = null
-    private var matrix_1_1Btn: Button? = null
-    private var matrix_9_16Btn: Button? = null
     private var textureRenderer: TextureRenderer? = null
     private var bitmap: Bitmap? = null
 
@@ -43,37 +38,9 @@ class TextureActivity : AppCompatActivity() {
         glSurfaceView?.setOnClickListener(l)
         glSurfaceView?.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
 
-//        nonMatrixBtn = findViewById(R.id.texture_matrix_identity)
-//        matrix_1_1Btn = findViewById(R.id.texture_matrix_1_1)
-//        matrix_4_3Btn = findViewById(R.id.texture_matrix_1_1)
-//        matrix_9_16Btn = findViewById(R.id.texture_matrix_1_1)
-
-        nonMatrixBtn?.setOnClickListener(l)
-        matrix_1_1Btn?.setOnClickListener(l)
-        matrix_4_3Btn?.setOnClickListener(l)
-        matrix_9_16Btn?.setOnClickListener(l)
     }
 
     private val l = View.OnClickListener {
-        if (it === nonMatrixBtn) {
-            textureRenderer?.setTextureMatrixType(TextureRenderer.TextureScaleType.IDENTITY)
-            glSurfaceView?.requestRender()
-        }
-
-        if (it === matrix_1_1Btn) {
-            textureRenderer?.setTextureMatrixType(TextureRenderer.TextureScaleType.SCALE_1_1)
-            glSurfaceView?.requestRender()
-        }
-
-        if (it === matrix_4_3Btn) {
-            textureRenderer?.setTextureMatrixType(TextureRenderer.TextureScaleType.SCALE_4_3)
-            glSurfaceView?.requestRender()
-        }
-
-        if (it === matrix_9_16Btn) {
-            textureRenderer?.setTextureMatrixType(TextureRenderer.TextureScaleType.SCALE_9_16)
-            glSurfaceView?.requestRender()
-        }
 
         if (it === glSurfaceView) {
             val width = bitmap?.width
@@ -114,40 +81,27 @@ class TextureActivity : AppCompatActivity() {
 
     private fun showTextureDialog() {
 
-//        val arrayOf = arrayOf("原始（无）", "1:1", "4:3", "9:16")
-//        val type = arrayOf(
-//            TextureRenderer.TextureScaleType.IDENTITY,
-//            TextureRenderer.TextureScaleType.SCALE_1_1,
-//            TextureRenderer.TextureScaleType.SCALE_4_3,
-//            TextureRenderer.TextureScaleType.SCALE_9_16
-//        )
-
         val hashMap = HashMap<String, Int>()
-        hashMap.put("原始（无）", TextureRenderer.TextureScaleType.IDENTITY)
-        hashMap.put("1:1", TextureRenderer.TextureScaleType.SCALE_1_1)
-        hashMap.put("4:3", TextureRenderer.TextureScaleType.SCALE_4_3)
-        hashMap.put("9:16", TextureRenderer.TextureScaleType.SCALE_9_16)
+        hashMap["原始（无）"] = TextureRenderer.TextureScaleType.IDENTITY
+        hashMap["1:1"] = TextureRenderer.TextureScaleType.SCALE_1_1
+        hashMap["4:3"] = TextureRenderer.TextureScaleType.SCALE_4_3
+        hashMap["9:16"] = TextureRenderer.TextureScaleType.SCALE_9_16
         val keys = hashMap.keys
         val toList = keys.toList()
         val toTypedArray = toList.toTypedArray()
-
 
         val builder = AlertDialog.Builder(this)
         builder.setTitle("纹理矩阵单选框")
             .setSingleChoiceItems(toTypedArray, -1) { dialog, which ->
 
-                val type:Int = hashMap[toTypedArray[which]]!!
-                textureRenderer?.setTextureMatrixType(type/*TextureRenderer.TextureScaleType.IDENTITY*/)
+                val type: Int = hashMap[toTypedArray[which]]!!
+                textureRenderer?.setTextureMatrixType(type)
                 glSurfaceView?.requestRender()
+
             }
             .setNegativeButton("取消", null)
             .create()
             .show()
     }
-
-    private fun switchTextureMatrixType() {
-
-    }
-
 
 }
